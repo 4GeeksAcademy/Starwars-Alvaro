78ea49fad8a4a3c7dd0c5ef6c7541357f5c347e4
Alvaro <alvaro.albalad@gmail.com>
2024-11-23 12:38:53 +0000

    guardado

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,10 +4,13 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light pt-0">
-			<div className="logo">
-				<img className="img-logo" src="https://imgs.search.brave.com/r8bm-WT1spDGncquBPGvD6ChyEC7j8bSqTTtQC8fkDA/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvZmVhdHVy/ZWQvNGstc3Rhci13/YXJzLTViaW5yZWl0/OWR1MjhiaWYuanBn"
-					alt="Logo de la web" />
-			</div>
+			<Link to="/">
+				<div className="logo">
+					<img className="img-logo" src="https://imgs.search.brave.com/r8bm-WT1spDGncquBPGvD6ChyEC7j8bSqTTtQC8fkDA/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvZmVhdHVy/ZWQvNGstc3Rhci13/YXJzLTViaW5yZWl0/OWR1MjhiaWYuanBn"
+						alt="Logo de la web" />
+				</div>
+			</Link>
+
 		</nav>
 	);
 };
